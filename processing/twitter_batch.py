e2ebf9a3fb45c00d0aba3e57aeb9c787abad46eb
Konniam Chan <konniamchan@me.com>
2015-12-12 15:59:26 -0800

    Revised postgres setup for 9.4. Added batch processing script.

---
+++ processing/twitter_batch.py
@@ -0,0 +1,65 @@
+# Batch process tweets in MongoDB to pull out counts of keywords
+import psycopg2 as pg
+import pymongo as pm
+import datetime as dt
+import re
+
+# Keyword terms
+kwd_terms = [r'lost +(my|his|her|your|their)? *job',
+             r'got *fired',r'unemploy(ed)?',r'lay(ed )?-?off']
+
+# Postgres connection
+conn    = pg.connect(user='w205',database='w205project',password='pw')
+cur     = conn.cursor()
+
+# Mongo connection
+client      = pm.MongoClient()
+db          = client["w205"]
+tweets_db   = db["twitter"]
+# deleted_db  = db["deleted"]
+
+# Initialize PostgreSQL table
+start_time = dt.datetime(2015, 12, 1, 0)
+end_time = dt.datetime(2015, 12, 11, 0)
+time = start_time
+
+while time <= end_time:
+    # Total count
+    cur.execute("INSERT INTO total_tweets VALUES (%s, %s)", (time.isoformat(), 0))
+    conn.commit()
+    # Keyword count
+    for k in kwd_terms:
+        cur.execute("INSERT INTO keyword_tweets_cnt VALUES (%s, %s, %s)", (time.isoformat(), k, 0))
+        conn.commit()
+    time = time + dt.timedelta(hours=1)
+    
+Iterate through tweets to record counts
+for tweet in tweets_db.find()[:50]:
+    # Parse and group into hour
+    created_time = dt.datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S +0000 %Y')
+    created_time = created_time.replace(minute=0,second=0,microsecond=0)
+
+    # Get total count
+    cur.execute("SELECT count FROM total_tweets WHERE timestamp = %s", (created_time.isoformat(), ))
+    total_count = cur.fetchone()
+    conn.commit()
+
+    # Update total count        
+    cur.execute("UPDATE total_tweets SET count=%s WHERE timestamp = %s", (total_count[0]+1, created_time))
+    conn.commit()
+
+    # Check if tweet contains keyword
+    for k in kwd_terms:
+        if re.search(k, tweet['text']):
+
+            # Get current count
+            cur.execute("SELECT count FROM keyword_tweets_cnt WHERE timestamp = %s AND kword_search = %s", (created_time.isoformat(), k))
+            keyword_count = cur.fetchone()
+            conn.commit()
+
+            # Update keyword
+            cur.execute("UPDATE keyword_tweets_cnt SET count=%s WHERE timestamp = %s AND kword_search = %s", (keyword_count[0]+1, created_time, k))
+            conn.commit()
+
+cur.close()
+conn.close()
